f156009ee932b37add70923ed5cbfa4ca345b759
Samind2 <suphaneerangsirat@gmail.com>
2023-01-27 12:01:12 +0700

    27/1/66

---
+++ Exercise22.java
@@ -0,0 +1,12 @@
+public class Exercise22 {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=12; i++){
+            int n = 8;
+                System.out.printf("%5dx%2d=%3d", n, i, n*i);
+                n++;
+            }
+            System.out.println();
+        }
+    }
+
